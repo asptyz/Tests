0822c3a3285fd1e0dd7f85d37cbfbda78bacdee0
Ayyappa <148414465+batsinimunh@users.noreply.github.com>
2025-02-05 15:59:41 -0500

    upto Addition part is done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,9 +21,19 @@ def index():
         task_content = request.form['content']
         new_task = Todo(content = task_content)
     
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
         
+        except:
+            return "there was an issue adiding your task"
+            
     else:
-        return render_template('index.html')
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks = tasks)
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
