f7951bf7d480b73dc09cd7d0e92e1e2263085f2a
Ayyappa <148414465+batsinimunh@users.noreply.github.com>
2025-02-05 14:24:03 -0500

    took long enough time to lock in hehehe all bark no bite

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods= ['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content = task_content)
+    
+        
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(port = 8000,debug = True)
+    app.run(debug = True)
